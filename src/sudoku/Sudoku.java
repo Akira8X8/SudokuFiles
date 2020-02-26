538ef632ff2e205385180dec82a4ccb307cd069a
Zed <akira8x8@gmail.com>
2020-02-25 22:29:22 -0500

    Set up game board and display method

--- src/sudoku/Sudoku.java
+++ src/sudoku/Sudoku.java
@@ -1,25 +1,29 @@
 package sudoku;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Scanner;
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Sudoku {
 
-    private int[][] gameBoard;
+    private int[][] gameBoard = new int[9][9];
     private Scanner keyboard;
-    
+
     //Rules: chosen number cannot be repeated in same row
     //or column
     // or 3x3 group, from 0-2, 3-5, 6-9
-
     public static void main(String[] args) {
         new Sudoku();
     }
-    
-    public Sudoku(){
+
+    public Sudoku() {
         // initialize game
         initializeGame();
         printGameBoard();
@@ -38,21 +42,45 @@ public class Sudoku {
             }
         }
     }
-    
-    private void initializeGame(){
+
+    private void initializeGame() {
         //Opens a text file containing a 9x9 space-separated integer numbers representing a single sudoku game. After reading it, the method places those numbers in the global gameBoard 2D variable. 
+        File gameFile = new File("C:\\Users\\Zed\\Documents\\NetBeansProjects\\Sudoku\\src\\sudoku\\sudokuSample.txt");
+        try {
+        Scanner gameScanner = new Scanner(gameFile);
+        while (gameScanner.hasNextLine()) {
+            for (int i = 0; i < gameBoard.length; i++) {
+                String[] line = gameScanner.nextLine().trim().split(" ");
+                for (int j = 0; j < line.length; j++) {
+                    gameBoard[i][j] = Integer.parseInt(line[j]);
+                }
+            }
+        }
+//        System.out.println(Arrays.deepToString(gameBoard));
+        } catch (FileNotFoundException ex) {
+            System.out.println("File not found.  Check path.");
+        }
     }
-    
-    private void printGameBoard(){
+
+    private void printGameBoard() {
         //Description: Prints the game board in the terminal window. Later in this course we will implement it in a GUI app.
-
+        System.out.println("Current gameboard:");
+        for (int i = 0; i < gameBoard.length; i++) {
+            for (int j = 0; j < gameBoard.length; j++) {
+                if (j == gameBoard.length - 1) {
+                    System.out.print(gameBoard[i][j] + "\t\n");
+                } else {
+                    System.out.print(gameBoard[i][j] + "  ");
+                }
+            }
+        }
     }
-    
-    private boolean hasGameBeenSolved(){
+
+    private boolean hasGameBeenSolved() {
         //Description: Returns true if the game has been completely solved; false otherwise.Hint, loop over all the cells and if you find one cell that has 0 returns false immediately. If after looping over all the cells you could not find any cell with 0, then returns true (game has been solved since there is no empty cells left).
-        return false;
+        
+        return true; 
     }
-
 
     private int[] getNextCell() {
         for (int row = 0; row < 9; row++) {
@@ -73,44 +101,42 @@ public class Sudoku {
         }
         return null;
     }
-    
-    private Set<Integer> getFirstRuleElimination(int row){
+
+    private Set<Integer> getFirstRuleElimination(int row) {
         //Description: Loops over all the columns of the given row (method input) and adds all the numbers found in these cells into a Set object. Returns this set object.
         Set<Integer> set = new HashSet();
         return set;
     }
-    
-    private Set<Integer> getSecondRuleElimination(int row){
+
+    private Set<Integer> getSecondRuleElimination(int row) {
         //Loops over all the rows of the given column (method input) and adds all the numbers found in these cells into a Set object. Returns this set object.
         Set<Integer> set = new HashSet();
         return set;
     }
-    
-    private Set<Integer> getThirdRuleElimination(int row, int column){
+
+    private Set<Integer> getThirdRuleElimination(int row, int column) {
         //Each cell defined by a (row, column) belongs to a group (A, B, C, … as shown in the second figure of this assignment). This method shall initially call two methods (see description about the methods below): one returning an array with min and max rows and the other with min and max columns for the current group that this cell defined by a rowcol belongs to. And then, loop over the 9 cells defined in this group (2 Nested FOR loops) and collect all the numbers found in the gameboard into a Set object and return that object.
         Set<Integer> set = new HashSet();
         return set;
     }
-    
+
     private int[] getRowRangeForGroup(int row) {
         //Based on the given row, it defines the minimum and maximum row for that group. Example for row = 1 then min and max row for this group is 0 and 2 respectively, while for row = 4 then min and max rows are 3 and 5. This method returns an integer array with those two numbers.
         int[] rowRange = new int[2];
         return rowRange;
     }
-    
+
     private int[] getColRangeForGroup(int row) {
-        
+
         int[] colRange = new int[2];
         return colRange;
     }
-    
+
     private Set<Integer> getSurvivors(Set<Integer> firstRuleElimination,
-Set<Integer> secondRuleElimination, Set<Integer> thirdRuleElimination) {
+            Set<Integer> secondRuleElimination, Set<Integer> thirdRuleElimination) {
         Set<Integer> survivors = new HashSet();
         return survivors;
         //This method does two tasks: first it creates a temporary Set and add all the sets given in the method input into it (a consolidated set with all non-allowed numbers found when applying the 3 rules). Then it creates another Set that is the opposite of this consolidated set, i.e., contains only the allowed numbers for the given current cell.
     }
-
-
 
 }
